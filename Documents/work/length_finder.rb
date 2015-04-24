96ac37d56640dbbff9914fe5d7249d756e181f92
Lexi Mithel <lexi.mithel22@gmail.com>
2015-04-23 19:50:55 -0700

    oops

--- Documents/work/length_finder.rb
+++
@@ -1,5 +0,0 @@
-def length_finder(input_array)
-  puts input_array.map {|i| i.length}
-end
-
-length_finder(["abcd","bye"])
--- Documents/work/non_duplicated_values.rb
+++
@@ -1,5 +0,0 @@
-def non_duplicated_values(values)
-  puts values.find_all { |i| values.count(i) == 1 }
-end
-
-non_duplicated_values([1,2,2,4])
